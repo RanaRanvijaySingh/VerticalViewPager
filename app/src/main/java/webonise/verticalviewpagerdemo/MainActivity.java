5627e39d27d057e074c591d49815a9636348838f
Rana Ranvijay SIngh <ranaranvijaysingh@Venkateshs-Mac-mini.local>
2017-02-03 12:41:32 +0530

    Flavours with different environment is working.

--- app/src/main/java/webonise/verticalviewpagerdemo/MainActivity.java
+++ app/src/main/java/webonise/verticalviewpagerdemo/MainActivity.java
@@ -2,6 +2,7 @@ package webonise.verticalviewpagerdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,11 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(Constants.ENVIRONMENT);
         initializeViewPager();
     }
 
     private void initializeViewPager() {
-        final VerticalViewPager verticalViewPager = (VerticalViewPager)findViewById(R.id.viewPager);
+        final VerticalViewPager verticalViewPager = (VerticalViewPager) findViewById(R.id.viewPager);
         final PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
         verticalViewPager.setAdapter(pagerAdapter);
     }
---
+++ app/src/prod/java/webonise/verticalviewpagerdemo/Constants.java
@@ -0,0 +1,5 @@
+package webonise.verticalviewpagerdemo;
+
+public class Constants {
+    public static final String ENVIRONMENT = "Production";
+}
